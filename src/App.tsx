bb472885404732e66b12ad66a6e244d70c7f5136
Hayder Jebur <hayderjabur1983@gmail.com>
2021-02-10 21:57:02 -0800

    add resove function

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import * as esbuild from 'esbuild-wasm';
 
 import './App.css';
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 
 function App() {
   const [input, setInput] = useState('');
   const [code, setCode] = useState('');
 
-  const onClick = () => {
-    console.log(input);
+  const ref = useRef<any>();
+
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: '/esbuild.wasm',
+    });
+  };
+
+  useEffect(() => {
+    startService();
+  }, []);
+
+  const onClick = async () => {
+    if (!ref.current) {
+      return;
+    }
+    const result = await ref.current.build({
+      entryPoints: ['index.js'],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin()],
+    });
+    console.log(result);
+
+    setCode(result.outputFiles[0].text);
   };
   return (
     <div className='App'>
