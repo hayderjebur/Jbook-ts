9812fcf41ad003a3bebef16c468c09d54029754f
Hayder Jebur <hayderjabur1983@gmail.com>
2021-02-12 19:27:46 -0800

    add fetch plugin file

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,18 @@ import * as esbuild from 'esbuild-wasm';
 
 import './App.css';
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
+import { fetchPlugin } from './plugins/fetch-plugin';
 
 function App() {
   const [input, setInput] = useState('');
-  const [code, setCode] = useState('');
 
   const ref = useRef<any>();
+  const iframe = useRef<any>();
 
   const startService = async () => {
     ref.current = await esbuild.startService({
       worker: true,
-      wasmURL: '/esbuild.wasm',
+      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
     });
   };
 
@@ -25,20 +26,44 @@ function App() {
     if (!ref.current) {
       return;
     }
+
+    iframe.current.srcdoc = html;
+
     const result = await ref.current.build({
       entryPoints: ['index.js'],
       bundle: true,
       write: false,
-      plugins: [unpkgPathPlugin()],
+      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
       define: {
         'process.env.NODE_ENV': '"production"',
         global: 'window',
       },
     });
-    console.log(result);
 
-    setCode(result.outputFiles[0].text);
+    // setCode(result.outputFiles[0].text);
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
   };
+
+  const html = `
+ <html>
+  <head>
+    <body>
+      <div id="root"></div>
+      <script>
+        window.addEventListener('message', (event)=>{
+          try{
+
+            eval(event.data)
+          }catch(err){
+            const root = document.querySelector('#root')
+            root.innerHTML='<div style="color:red"><h4>Runtime Error</h4>' + err + '</div>'
+          }
+        },false)
+      </script>
+    </body>
+  </head>
+ </html>
+  `;
   return (
     <div className='App'>
       <textarea
@@ -48,7 +73,8 @@ function App() {
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
-      <pre>{code}</pre>
+
+      <iframe ref={iframe} sandbox='allow-scripts' srcDoc={html} title='test' />
     </div>
   );
 }
