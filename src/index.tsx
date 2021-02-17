c96c4edb96fa73d1f39983d2c71374ad49137f04
Hayder Jebur <hayderjabur1983@gmail.com>
2021-02-16 18:44:33 -0800

    add styling

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import 'bulmaswatch/superhero/bulmaswatch.min.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
