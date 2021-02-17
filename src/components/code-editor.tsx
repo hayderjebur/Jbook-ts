2dee50360f602910d528500cf597f53ec7068de0
Hayder Jebur <hayderjabur1983@gmail.com>
2021-02-16 22:32:11 -0800

    layout preview component and bundle component

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -54,7 +54,7 @@ const CodeEditor: React.FC<CodeEditorProps> = ({ initialValue, onChange }) => {
         }}
         theme='dark'
         language='javascript'
-        height='500px'
+        height='100%'
       />
     </div>
   );
--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -1,39 +1,52 @@
-import { useEffect, useRef } from 'react';
+import './preview.css';
+import { useRef, useEffect } from 'react';
 
 interface PreviewProps {
   code: string;
 }
 
 const html = `
- <html>
-  <head>
-    <body>
-      <div id="root"></div>
-      <script>
-        window.addEventListener('message', (event)=>{
-          try{
-
-            eval(event.data)
-          }catch(err){
-            const root = document.querySelector('#root')
-            root.innerHTML='<div style="color:red"><h4>Runtime Error</h4>' + err + '</div>'
-          }
-        },false)
-      </script>
-    </body>
-  </head>
- </html>
+    <html>
+      <head>
+        <style>html { background-color: white; }</style>
+      </head>
+      <body>
+        <div id="root"></div>
+        <script>
+          window.addEventListener('message', (event) => {
+            try {
+              eval(event.data);
+            } catch (err) {
+              const root = document.querySelector('#root');
+              root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
+              console.error(err);
+            }
+          }, false);
+        </script>
+      </body>
+    </html>
   `;
+
 const Preview: React.FC<PreviewProps> = ({ code }) => {
   const iframe = useRef<any>();
 
   useEffect(() => {
     iframe.current.srcdoc = html;
-    iframe.current.contentWindow.postMessage(code, '*');
+    setTimeout(() => {
+      iframe.current.contentWindow.postMessage(code, '*');
+    }, 50);
   }, [code]);
 
   return (
-    <iframe ref={iframe} sandbox='allow-scripts' srcDoc={html} title='test' />
+    <div className='preview-wrapper'>
+      <iframe
+        title='preview'
+        ref={iframe}
+        sandbox='allow-scripts'
+        srcDoc={html}
+      />
+    </div>
   );
 };
+
 export default Preview;
