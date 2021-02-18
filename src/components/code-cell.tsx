cd8252cc793a78d04ab264519185900656c30913
Hayder Jebur <hayderjabur1983@gmail.com>
2021-02-17 18:29:54 -0800

    add handle error

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -7,11 +7,13 @@ import Resizable from './resizable';
 function CodeCell() {
   const [input, setInput] = useState('');
   const [code, setCode] = useState('');
+  const [err, setErr] = useState('');
 
   useEffect(() => {
     const timer = setTimeout(async () => {
       const output = await bundle(input);
-      setCode(output);
+      setCode(output.code);
+      setCode(output.err);
     }, 1000);
     return () => {
       clearTimeout(timer);
@@ -27,7 +29,7 @@ function CodeCell() {
             onChange={(value) => setInput(value)}
           />
         </Resizable>
-        <Preview code={code} />
+        <Preview code={code} err={err} />
       </div>
     </Resizable>
   );
