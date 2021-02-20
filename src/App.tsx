1b94c737119f9b28472471b3cfda8ff7555f5f4a
Hayder Jebur <hayderjabur1983@gmail.com>
2021-02-19 21:49:02 -0800

    add style to text-editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import CodeCell from './components/code-cell';
+import TextEditor from './components/text-editor';
 
 function App() {
   return (
     <div className='App'>
-      <CodeCell />
+      <TextEditor />
+      {/* <CodeCell /> */}
     </div>
   );
 }
