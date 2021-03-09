c6831dac428d848920cac9fe5da595feb94734b2
Hayder Jebur <hayderjabur1983@gmail.com>
2021-03-08 23:28:48 -0800

    add cell-list and cell-list-item

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import { Provider } from 'react-redux';
 import { store } from './state/store';
-
-import TextEditor from './components/text-editor';
+import CellList from './components/cell-list';
 
 function App() {
   return (
     <Provider store={store}>
       <div>
-        <TextEditor />
+        <CellList />
       </div>
     </Provider>
   );
